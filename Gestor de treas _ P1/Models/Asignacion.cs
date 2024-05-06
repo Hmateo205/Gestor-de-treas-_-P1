51a07dcd87c19b3a1082fc538d15e22a2dfdfe3e
Hernan Mateo Jurado Moran <hernan.jurado@udla.edu.ec>
2024-05-06 07:50:11 -0500

    Modelo Asignacion

---
+++ Gestor de treas _ P1/Models/Asignacion.cs
@@ -0,0 +1,11 @@
+﻿namespace Gestor_de_treas___P1.Models
+{
+    public class Asignacion
+    {
+        public int AsignacionId { get; set; }
+        public int TareaId { get; set; }
+        public Tarea Tarea { get; set; }
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+    }
+}
