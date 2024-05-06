bd49375ac61e6c71e4e566683f41834faca10e72
Hernan Mateo Jurado Moran <hernan.jurado@udla.edu.ec>
2024-05-06 07:38:18 -0500

    Modelo Tarea

---
+++ Gestor de treas _ P1/Models/Tarea.cs
@@ -0,0 +1,14 @@
+﻿namespace Gestor_de_treas___P1.Models
+{
+    public class Tarea
+    {
+        public int TareaId { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaLimite { get; set; }
+        public Prioridad Prioridad { get; set; }
+        public Estado Estado { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
