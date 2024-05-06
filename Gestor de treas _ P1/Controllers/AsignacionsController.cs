be0407159a6395ca563f245d61a44c77be0090f0
Hernan Mateo Jurado Moran <hernan.jurado@udla.edu.ec>
2024-05-06 08:08:35 -0500

    Controlador Etiuetas

---
+++ Gestor de treas _ P1/Controllers/AsignacionsController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Gestor_de_treas___P1.Data;
+using Gestor_de_treas___P1.Models;
+
+namespace Gestor_de_treas___P1.Controllers
+{
+    public class AsignacionsController : Controller
+    {
+        private readonly Gestor_de_treas___P1Context _context;
+
+        public AsignacionsController(Gestor_de_treas___P1Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Asignacions
+        public async Task<IActionResult> Index()
+        {
+            var gestor_de_treas___P1Context = _context.Asignacion.Include(a => a.Tarea).Include(a => a.Usuario);
+            return View(await gestor_de_treas___P1Context.ToListAsync());
+        }
+
+        // GET: Asignacions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var asignacion = await _context.Asignacion
+                .Include(a => a.Tarea)
+                .Include(a => a.Usuario)
+                .FirstOrDefaultAsync(m => m.AsignacionId == id);
+            if (asignacion == null)
+            {
+                return NotFound();
+            }
+
+            return View(asignacion);
+        }
+
+        // GET: Asignacions/Create
+        public IActionResult Create()
+        {
+            ViewData["TareaId"] = new SelectList(_context.Tarea, "TareaId", "TareaId");
+            ViewData["UsuarioId"] = new SelectList(_context.Set<Usuario>(), "UsuarioId", "UsuarioId");
+            return View();
+        }
+
+        // POST: Asignacions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AsignacionId,TareaId,UsuarioId")] Asignacion asignacion)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(asignacion);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TareaId"] = new SelectList(_context.Tarea, "TareaId", "TareaId", asignacion.TareaId);
+            ViewData["UsuarioId"] = new SelectList(_context.Set<Usuario>(), "UsuarioId", "UsuarioId", asignacion.UsuarioId);
+            return View(asignacion);
+        }
+
+        // GET: Asignacions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var asignacion = await _context.Asignacion.FindAsync(id);
+            if (asignacion == null)
+            {
+                return NotFound();
+            }
+            ViewData["TareaId"] = new SelectList(_context.Tarea, "TareaId", "TareaId", asignacion.TareaId);
+            ViewData["UsuarioId"] = new SelectList(_context.Set<Usuario>(), "UsuarioId", "UsuarioId", asignacion.UsuarioId);
+            return View(asignacion);
+        }
+
+        // POST: Asignacions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AsignacionId,TareaId,UsuarioId")] Asignacion asignacion)
+        {
+            if (id != asignacion.AsignacionId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(asignacion);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AsignacionExists(asignacion.AsignacionId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TareaId"] = new SelectList(_context.Tarea, "TareaId", "TareaId", asignacion.TareaId);
+            ViewData["UsuarioId"] = new SelectList(_context.Set<Usuario>(), "UsuarioId", "UsuarioId", asignacion.UsuarioId);
+            return View(asignacion);
+        }
+
+        // GET: Asignacions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var asignacion = await _context.Asignacion
+                .Include(a => a.Tarea)
+                .Include(a => a.Usuario)
+                .FirstOrDefaultAsync(m => m.AsignacionId == id);
+            if (asignacion == null)
+            {
+                return NotFound();
+            }
+
+            return View(asignacion);
+        }
+
+        // POST: Asignacions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var asignacion = await _context.Asignacion.FindAsync(id);
+            if (asignacion != null)
+            {
+                _context.Asignacion.Remove(asignacion);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AsignacionExists(int id)
+        {
+            return _context.Asignacion.Any(e => e.AsignacionId == id);
+        }
+    }
+}
---
+++ Gestor de treas _ P1/Controllers/EtiquetasController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Gestor_de_treas___P1.Data;
+using Gestor_de_treas___P1.Models;
+
+namespace Gestor_de_treas___P1.Controllers
+{
+    public class EtiquetasController : Controller
+    {
+        private readonly Gestor_de_treas___P1Context _context;
+
+        public EtiquetasController(Gestor_de_treas___P1Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Etiquetas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Etiqueta.ToListAsync());
+        }
+
+        // GET: Etiquetas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var etiqueta = await _context.Etiqueta
+                .FirstOrDefaultAsync(m => m.EtiquetaId == id);
+            if (etiqueta == null)
+            {
+                return NotFound();
+            }
+
+            return View(etiqueta);
+        }
+
+        // GET: Etiquetas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Etiquetas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EtiquetaId,Nombre,Color")] Etiqueta etiqueta)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(etiqueta);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(etiqueta);
+        }
+
+        // GET: Etiquetas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var etiqueta = await _context.Etiqueta.FindAsync(id);
+            if (etiqueta == null)
+            {
+                return NotFound();
+            }
+            return View(etiqueta);
+        }
+
+        // POST: Etiquetas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EtiquetaId,Nombre,Color")] Etiqueta etiqueta)
+        {
+            if (id != etiqueta.EtiquetaId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(etiqueta);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EtiquetaExists(etiqueta.EtiquetaId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(etiqueta);
+        }
+
+        // GET: Etiquetas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var etiqueta = await _context.Etiqueta
+                .FirstOrDefaultAsync(m => m.EtiquetaId == id);
+            if (etiqueta == null)
+            {
+                return NotFound();
+            }
+
+            return View(etiqueta);
+        }
+
+        // POST: Etiquetas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var etiqueta = await _context.Etiqueta.FindAsync(id);
+            if (etiqueta != null)
+            {
+                _context.Etiqueta.Remove(etiqueta);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EtiquetaExists(int id)
+        {
+            return _context.Etiqueta.Any(e => e.EtiquetaId == id);
+        }
+    }
+}
