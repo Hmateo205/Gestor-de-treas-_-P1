d4245f86c12c0ce1d223d07883675f3660242e5c
Hernan Mateo Jurado Moran <hernan.jurado@udla.edu.ec>
2024-05-06 07:46:17 -0500

    modelo Tarea Etiqueta

---
+++ Gestor de treas _ P1/Models/TareaEtiqueta.cs
@@ -0,0 +1,10 @@
+﻿namespace Gestor_de_treas___P1.Models
+{
+    public class TareaEtiqueta
+    {
+        public int TareaId { get; set; }
+        public Tarea Tarea { get; set; }
+        public int EtiquetaId { get; set; }
+        public Etiqueta Etiqueta { get; set; }
+    }
+}
